1a314cf02ff21c390fc924a23861d26c4b4188f2
Nety <netanielw@gmail.com>
2021-10-24 18:33:52 +0300

    favicon and some effects

--- my-tasks-manager/src/components/Home.js
+++ my-tasks-manager/src/components/Home.js
@@ -2,50 +2,60 @@
 import Card from 'react-bootstrap/Card'
 import {Link} from 'react-router-dom'
 import Footer from './Footer'
+import Fade from 'react-reveal/Fade';
+import Flip from 'react-reveal/Flip';
 
 function Home() {
     return(
         <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', overflowX: 'hidden'}}>
          
             <div className='image'>
-                <h1 className='titleHome'>TROLLEO</h1>
+                <Fade bottom>
+                    <h1 className='titleHome'>TROLLEO</h1>
+                </Fade>
             </div>
             <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
                 <div>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/4149/4149646.png" />
-                        <Card.Body>
-                            <Card.Title>Manage your tasks</Card.Title>
-                            <Card.Text>
-                                When you have so many task and you dont know where to start, we are here for you!
-                            </Card.Text>
-                           
-                        </Card.Body>
-                    </Card>
+                    <Fade left>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/4149/4149646.png" />
+                            <Card.Body>
+                                <Card.Title>Manage your tasks</Card.Title>
+                                <Card.Text>
+                                    When you have so many task and you dont know where to start, we are here for you!
+                                </Card.Text>
+                            
+                            </Card.Body>
+                        </Card>
+                    </Fade>
                 </div>
                 <div>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/4753/4753192.png" />
-                        <Card.Body>
-                            <Card.Title>Save Time</Card.Title>
-                            <Card.Text>
-                                You always have your tasks close so no more wasting time thinking what you have to do.
-                            </Card.Text>
-                           
-                        </Card.Body>
-                    </Card>
+                    <Fade left>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/4753/4753192.png" />
+                            <Card.Body>
+                                <Card.Title>Save Time</Card.Title>
+                                <Card.Text>
+                                    You always have your tasks close so no more wasting time thinking what you have to do.
+                                </Card.Text>
+                            
+                            </Card.Body>
+                        </Card>
+                    </Fade>
                 </div>
                 <div>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2799/2799195.png" />
-                        <Card.Body>
-                            <Card.Title>Control your Workflow</Card.Title>
-                            <Card.Text>
-                                Being organized will help you increase you productivity. <br/>Say tanks later.
-                            </Card.Text>
-                           
-                        </Card.Body>
-                    </Card>
+                    <Fade left>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2799/2799195.png" />
+                            <Card.Body>
+                                <Card.Title>Control your Workflow</Card.Title>
+                                <Card.Text>
+                                    Being organized will help you increase you productivity. <br/>Say tanks later.
+                                </Card.Text>
+                            
+                            </Card.Body>
+                        </Card>
+                    </Fade>
                 </div>
             </div>
             <div className='someText'>
@@ -54,7 +64,9 @@ function Home() {
                     Create many tables and tasks as you want. <br/> Organize them by status: To do, In progress and Done. <br/> 
                     Try out. Its free!
                 </h5>
-                <Link style={{marginTop: '25px'}} to='/register' className='margin Mybtn'>Let's Start</Link>
+                <Flip right>
+                    <Link style={{marginTop: '25px'}} to='/register' className='margin Mybtn'>Let's Start</Link>
+                </Flip>
             </div>
             <Footer/>
         </div>     
